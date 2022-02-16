4b2eec5e54ac0922e89f198e59daf770c246b2f7
VilmaBety <96948541+VilmaBety@users.noreply.github.com>
2022-02-16 13:54:23 -0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello from Leo!");
+	System.out.println("Hello Leo, my name is Vilma ");    
+	    
     }
 }
